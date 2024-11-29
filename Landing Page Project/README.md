292a3ca28c4d8db5fc5dc82eee1bbe5500355595
Mahdi <133682620+mahdi204@users.noreply.github.com>
2024-11-29 14:51:23 +0200

    Add files via upload

---
+++ Landing Page Project/README.md
@@ -0,0 +1,38 @@
+# Project Name
+
+Landing Page Project
+
+# Quickstart/Demo
+
+A live demo is available here.
+
+# Table of Contents
+
+1-Project Title
+2-Quickstart/Demo
+3-Table of Contents
+4-Installation
+5-Usage
+6-Development
+7-Contribute
+8-License
+
+# Installation
+
+[(Back to top)](#table-of-contents)
+
+# Usage
+
+[(Back to top)](#table-of-contents)
+
+# Development
+
+[(Back to top)](#table-of-contents)
+
+# Contribute
+
+[(Back to top)](#table-of-contents)
+
+# License
+
+[(Back to top)](#table-of-contents)
---
+++ Landing Page Project/js/app.js
@@ -0,0 +1,47 @@
+const navList = document.getElementById("navbar__list");
+const sections = Array.from(document.querySelectorAll("section"));
+let numberOfListItems = sections.length;
+
+function createListItem(){
+    for(section of sections){
+        sectionName = section.getAttribute('data-nav');
+        sectionLink = section.getAttribute('id');
+        //create an item for each section
+        listItem = document.createElement("li");
+        //add the item text 
+        listItem.innerHTML = `<a href="#${sectionLink}" class="menu__link">${sectionName}</a>`;
+        //add listItem to the menu
+        navList.appendChild(listItem);
+    }
+}
+
+function sectionInViewPort(element){
+    let sectionPosition = element.getBoundingClientRect();
+    return (
+        sectionPosition.top >= 0 &&
+        sectionPosition.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    );
+}
+
+function toggleActiveClass(){
+    for(section of sections){
+        section.classList.remove("your-active-class");
+    }
+    let activeSection = null;
+    for (let section of sections) {
+        if (sectionInViewPort(section)) {
+            activeSection = section;
+        }
+    }
+
+    for (let section of sections) {
+        if (section === activeSection) {
+            section.classList.add("active");
+        } else {
+            section.classList.remove("active");
+        }
+    }
+}
+
+createListItem();
+document.addEventListener("scroll",toggleActiveClass);
